1663cde41975ffe13daaf8f8193f8abbf7ec538d
Ethan White <ethan@weecology.org>
2015-02-21 16:46:00 -0700

    Add reminder to filter out poorly sampled species

--- bbs_benchmarks.py
+++ bbs_benchmarks.py
@@ -24,6 +24,7 @@ def get_data(dataset):
     else:
         if not database_exists('bbs'):
             install_dataset('bbs')
+        #FIXME: This query doesn't currently deal with poorly sampled species (e.g., nocturnal)
         bbs_query = """SELECT (counts.statenum * 1000) + counts.route AS site_id, counts.year,
                        counts.aou AS species_id, counts.speciestotal AS abundance
                        FROM bbs.counts JOIN bbs.weather
