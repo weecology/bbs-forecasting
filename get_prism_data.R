9eae4b21edaa2b99c206bf5d753550c30ffd0238
Ethan White <ethan@weecology.org>
2015-07-10 13:08:46 -0400

    Add description to PRISM script

--- get_prism_data.R
+++ get_prism_data.R
@@ -1,3 +1,5 @@
+# Download and install PRISM data into PostgreSQL
+
 library(prism)
 options(prism.path = "./data/prismdata")
 get_prism_monthlys(type="tmean", year = 1990:2010, month = 6, keepZip=F)
