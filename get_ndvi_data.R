620c5dddbb1eff4351f4515c4845c9f1ef3a3207
Ethan White <ethan@weecology.org>
2015-07-16 12:31:33 -0400

    Only get NDVI data for BBS routes with contiguous data from 2000-2014

--- get_ndvi_data.R
+++ get_ndvi_data.R
@@ -2,8 +2,17 @@
 
 library(MODISTools)
 library(stringr)
+library(dplyr)
 
 route_data <- read.csv('data/BBS_routes.csv')
+route_data$site_id <-  route_data$statenum * 1000 + route_data$route
+
+contig_modern_routes <- read.csv('data/contig_modern_routes.csv')
+
+colnames(contig_modern_routes) <- c('site_id')
+route_data <- semi_join(route_data, contig_modern_routes)
+
+
 coord_data <- route_data[c('lati', 'loni')]
 coord_data <- unique(coord_data) # the Routes table has duplicate coordinate values for some reason
 colnames(coord_data) <- c('lat', 'long')
