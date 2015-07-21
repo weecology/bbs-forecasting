ecedde56aca8f49dc0df091fb6a36c8a4513ac90
Ethan White <ethan@weecology.org>
2015-07-21 15:42:48 -0400

    Download NDVI data for June in single year chunks
    This allows MODISTools::MODISSummaries to properly combine the data
    at the annual level for just the month of interest.

--- get_ndvi_data.R
+++ get_ndvi_data.R
@@ -15,12 +15,25 @@ route_data <- semi_join(route_data, contig_modern_routes)
 coord_data <- route_data[c('lati', 'loni')]
 coord_data <- unique(coord_data) # the Routes table has duplicate coordinate values for some reason
 colnames(coord_data) <- c('lat', 'long')
-coord_data$start.date <- rep(2000, nrow(coord_data))
-coord_data$end.date <- rep(2014, nrow(coord_data))
+coord_date_data <- coord_data
+coord_date_data$start.date <- rep(as.Date("2000-06-01"), nrow(coord_data))
+coord_date_data$end.date <- rep(as.Date("2000-06-30"), nrow(coord_data))
 
-unaquired_coord_data <- UpdateSubsets(LoadDat = coord_data, StartDate = TRUE,
-                                     Dir = "./data/modisdata/")
+years = 2001:2014
+for (year in years){
+  coord_data_year <- coord_data
+  coord_data_year$start.date <- rep(as.Date(paste(year, "-06-01", sep="")), nrow(coord_data_year))
+  coord_data_year$end.date <- rep(as.Date(paste(year, "-06-30", sep="")), nrow(coord_data_year))
+  coord_date_data <- rbind(coord_date_data, coord_data_year)
+}
+
+if (length(list.files("./data/modisdata/yearly/")) > 0){
+  unaquired_coord_data <- UpdateSubsets(LoadDat = coord_date_data, StartDate = TRUE,
+                                        Dir = "./data/modisdata/yearly/")
+} else {
+  unaquired_coord_data <- coord_date_data
+}
 
 MODISSubsets(LoadDat = unaquired_coord_data, Products = "MOD13Q1",
              Bands = c("250m_16_days_NDVI", "250m_16_days_pixel_reliability"),
-             Size = c(1,1), SaveDir = "./data/modisdata", StartDate = TRUE)
+             Size = c(0,0), SaveDir = "./data/modisdata/yearly/", StartDate = TRUE)
