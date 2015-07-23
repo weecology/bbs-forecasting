ca4bc7e0a1ea8a7a39b2e421282425dbdbb2e501
Ethan White <ethan@weecology.org>
2015-07-23 12:52:21 -0400

    Refactor NDVI acquisition and processing code into core + cleanup
    Also cleans out old time-series forecasting experiments

--- forecast-bbs-core.R
+++ forecast-bbs-core.R
@@ -66,3 +66,24 @@ get_filtered_ts <- function(df, min_ts_length){
   long_ts <- filter(contig_ts_length, n_years >= min_ts_length)
   contig_ts_long <- semi_join(contig_ts, long_ts)  
 }
+
+get_ndvi_ts_data <- function(tsdata){
+  modis_data_files <- list.files("./data/modisdata/", pattern = "MODIS_Data", full.names = TRUE)
+  if (length(modis_data_files) == 0){
+    tsdata_by_site_yr <- group_by(tsdata, site_id, lat, long, year)
+    richness_ndvi <- summarize(tsdata_by_site_yr, richness = n_distinct(species_id))
+    richness_ndvi$start.date <- paste(richness_ndvi$year, "-06-01", sep = "")
+    richness_ndvi$end.date <- paste(richness_ndvi$year, "-06-30", sep = "")
+    
+    output <- capture.output(
+                 MODISSummaries(LoadDat = richness_ndvi, Dir = "./data/modisdata/",
+                                Product = "MOD13Q1", Bands = c("250m_16_days_NDVI"),
+                                ValidRange = c(-2000,10000),
+                                NoDataFill = -3000, ScaleFactor = 0.0001, StartDate = TRUE)
+    )
+  }
+  modis_data_files <- list.files("./data/modisdata/", pattern = "MODIS_Data", full.names = TRUE)
+  richness_ndvi <- read.csv(modis_data_files[1])
+  colnames(richness_ndvi)[9] <- "ndvi"
+  return(richness_ndvi)
+}
