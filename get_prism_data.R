3f8f59a16abab3e6f5f1ced807580c4ab7871daf
Ethan White <ethan@weecology.org>
2015-09-18 08:51:29 -0400

    All modern PRISM data

--- get_prism_data.R
+++ get_prism_data.R
@@ -2,7 +2,7 @@
 
 library(prism)
 options(prism.path = "./data/prismdata")
-get_prism_monthlys(type="tmean", year = 1990:2010, month = 6, keepZip=F)
+get_prism_monthlys(type="tmean", year = 1981:2010, month = 6, keepZip=F)
 
 datapath = "./data/prismdata/"
 datadirs = dir(datapath)
