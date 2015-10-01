6f3aff8b7a72a2667354bf6ae5280f603e965542
Ethan White <ethan@weecology.org>
2015-10-01 09:28:41 -0400

    Extract PRISM data into data frame

--- get_prism_data.R
+++ get_prism_data.R
@@ -23,3 +23,21 @@ for (datadir in datadirs) {
   system(paste("raster2pgsql -s 4326", bil_file_path, datadir, ">", sql_file_path))
   system(paste("psql -d bbsforecasting -f", sql_file_path))
 }
+
+bbs_data <- read.csv("data/bbs_data.csv")
+locations <- unique(select(bbs_data, site_id, long, lat))
+coordinates(locations) <- c("long", "lat")
+#prism_files <- grep("_[0-9]{4}[0]",ls_prism_data()[,1],value=T)
+prism_stacked <- prism_stack(ls_prism_data())
+extracted <- raster::extract(prism_stacked, locations)
+prism_bbs_data <- data.frame(site_id = locations$site_id, coordinates(locations), extracted)
+prism_bbs_data <- prism_bbs_data %>%
+                    gather(date, value, 4:ncol(prism_bbs_data)) %>%
+                    tidyr::extract(date, c("clim_var", "year", "month"),
+                                   "PRISM_([:alpha:]*)_stable_[:alnum:]*_([:digit:]{4})([:digit:]{2})_") %>%
+                    spread(clim_var, value)
+prism_bbs_data$year <- as.numeric(prism_bbs_data$year)
+prism_bbs_data$month <- as.numeric(prism_bbs_data$month)
+database <- src_sqlite("./data/bbsforecasting.sqlite", create = TRUE)
+mydata <- copy_to(database, prism_bbs_data, temporary = FALSE,
+                  indexes = list(c("site_id", "year", "month")))
