f98b5b8e71cc193d0d90a18128e4129228f47452
Shawn Taylor <shawntaylor@weecology.org>
2016-02-11 10:42:18 -0500

    -fixed handling of getting prism data from sqlite -put in processing of bioclim vars from prism.

--- get_prism_data.R
+++ get_prism_data.R
@@ -69,11 +69,13 @@ check_If_Prism_Files_Present=function(prismLS, years){
 get_prism_data=function(){
   database <- src_sqlite("./data/bbsforecasting.sqlite", create = TRUE)
   #Query the prism data and check to see if it throws an error. 
-  prism_bbs_data=try(tbl(database, sql('SELECT * from prism_bbs_data')))
-  if(!class(prism_bbs_data)=='try-error'){
+  prism_bbs_data=try(collect(tbl(database, sql('SELECT * from prism_bbs_data'))))
+  
+  #If it works it should be of type list. If it doesn't then it should be of class 'try-error'
+  if(typeof(prism_bbs_data)=='list'){
     #If no error, return the data. 
-    return(prism_bbs_data)
-  } else { 
+    return(as.data.frame(prism_bbs_data))
+  } else if(class(prism_bbs_data)=='try-error') { 
     #If the query returned an error, assume it was because the table doesn't exist
     #and need's to be created/loaded from raw prism data. 
     
@@ -107,7 +109,78 @@ get_prism_data=function(){
     #Now return the data as asked for
     return(prism_bbs_data)
     
+  } else {
+    
+    stop('Im stopping because the sqlite query for prism_bbs data returned neither a data frame nor an error in get_prism_data()')
   }
   
 }
 
+
+#################################################################
+#From raw prism monthly values calculate all the bioclim variables.
+#You should not call this directly to load bioclim vars. Instead call
+#get_bioclim_data(), which will 1st try to load the data from the sdqlite
+#db before processing it all from scratch. 
+###################################################################
+#A quick helper function to calculate some of the bioclim variables,
+#like "precip in coldest month"
+maxMinCombo=function(vec1,vec2,max=TRUE){
+  #Return the value in vec1 in the position where
+  #vec2 is either highest or lowest. But 1st check for na 
+  #values. 
+  if(any(is.na(vec1)) | any(is.na(vec2))){
+    return(NA)
+  } else  if(max){
+    return(vec1[which.max(vec2)])
+  } else {
+    return(vec1[which.min(vec2)])
+  }
+}
+
+process_bioclim_data=function(){
+  #Get the prism data. It's pulled from the sqlite DB or downloaded as needed.
+  prism_bbs_data=get_prism_data()
+  
+  #Spread out the climate variables ppt, tmean, etc into columns
+  prism_bbs_data = prism_bbs_data %>%
+    spread(clim_var, value)
+  
+  #Process the quarter ones first.
+  quarterInfo=data.frame(month=1:12, quarter=c(1,1,1,2,2,2,3,3,3,4,4,4))
+  bioclimQuarterData= prism_bbs_data %>%
+    left_join(quarterInfo, by='month') %>%
+    group_by(site_id, year, quarter) %>%
+    summarize(precip=sum(ppt), temp=mean(tmean)) %>%
+    ungroup() %>%
+    group_by(site_id,year) %>%
+    summarize(bio8=maxMinCombo(temp, precip, max=TRUE),
+              bio9=maxMinCombo(temp, precip, max=FALSE),
+              bio10=max(temp),
+              bio11=min(temp),
+              bio16=max(precip),
+              bio17=min(precip),
+              bio18=maxMinCombo(precip, temp, max=TRUE),
+              bio19=maxMinCombo(precip, temp, max=FALSE)) %>%
+    ungroup()
+  
+  #Next the yearly ones, joining the quartely ones  back in at the end. 
+  bioclimData=prism_bbs_data %>%
+    group_by(site_id, year) %>%
+    mutate(monthlyTempDiff=tmax-tmin) %>%
+    summarize(bio1=mean(tmean),
+              bio2=mean(monthlyTempDiff),
+              bio4=sd(tmean)*100,
+              bio5=maxMinCombo(tmax,tmean,max=TRUE),
+              bio6=maxMinCombo(tmin,tmean,max=FALSE),
+              bio12=sum(ppt),
+              bio13=max(ppt),
+              bio14=min(ppt),
+              bio15=cv(ppt)) %>%
+    ungroup() %>%
+    mutate(bio7=bio5-bio6,
+           bio3=(bio2/bio7)*100) %>%
+    full_join(bioclimQuarterData, by=c('site_id','year'))
+  
+  return(bioclimData)
+}
