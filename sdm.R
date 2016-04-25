cc9e766c6c842e57c667b91d459444b9208af09e
David J. Harris <harry491@gmail.com>
2016-04-24 21:29:36 -0400

    spatial cross-validation

--- sdm.R
+++ sdm.R
@@ -20,6 +20,10 @@ min_num_yrs <- 25
 
 # start SDM ---------------------------------------------------------------
 
+n_test_years = 5
+train_years = start_yr:(end_yr - n_test_years)
+test_years = (end_yr - n_test_years + 1):end_yr
+
 library(gbm)
 library(viridis)
 
@@ -28,8 +32,6 @@ n_clusters = 100
 n_folds = 10
 stopifnot(n_clusters %% n_folds == 0)
 
-
-
 # Load data ---------------------------------------------------------------
 occurrences = get_bbs_data()
 env = get_env_data() %>%
@@ -37,13 +39,29 @@ env = get_env_data() %>%
   inner_join(distinct(occurrences[ , c("site_id", "lat", "long")]), 
              by = "site_id")
 
+# Determine presence-absence ----------------------------------------------
+
+sampling_events = tidyr::expand(occurrences, c(site_id, year)) %>%
+  inner_join(env, by = c("site_id", "year"))
+
+
+make_species_data = function(x){
+  occurrences %>% 
+    filter(species_id == x) %>% 
+    dplyr::select(site_id, year, abundance) %>%
+    right_join(sampling_events, by = c("site_id", "year")) %>%
+    replace_na(list(abundance = 0))
+}
+
 
 
 # Generate fold_id --------------------------------------------------------
 
 set.seed(0)
 
-locations = env %>% dplyr::select(site_id, lat, long) %>% distinct()
+locations = sampling_events %>% 
+  distinct(site_id) %>% 
+  dplyr::select(site_id, lat, long)
 
 cluster_id = kmeans(locations[ , 2:3], n_clusters, nstart = 100)$cluster
 
@@ -60,48 +78,43 @@ plot(
   pch = 16
 )
 
-
-# Determine presence-absence ----------------------------------------------
-
-sampling_events = tidyr::expand(occurrences, c(site_id, year)) %>%
-  inner_join(env, by = c("site_id", "year"))
-
-
-make_species_data = function(x){
-  occurrences %>% 
-    filter(species_id == x) %>% 
-    dplyr::select(site_id, year, abundance) %>%
-    right_join(sampling_events, by = c("site_id", "year")) %>%
-    replace_na(list(abundance = 0))
-}
+locations = cbind(locations, fold_id = fold_id)
 
 # fit gbm -----------------------------------------------------------------
-
-# NOTE: NOT USING THE CV FOLDS DEFINED ABOVE!
-
-data = make_species_data(x = 2890) %>%
+data = make_species_data(x = 7590) %>%
   mutate(presence = abundance > 0)
-g = gbm(
-  presence ~ .,
-  distribution = "bernoulli",
-  data = data %>% dplyr::select(-lat, -long, -site_id, -year, -abundance),
-  cv.folds = n_folds,
-  interaction.depth = 4,
-  n.trees = 1E4
-)
 
-p = predict(g, type = "response", n.trees = g$n.trees)
-plot(
-  jitter(data$lat, amount = 1/2) ~ jitter(data$long, amount = 1/2), 
-  col = rep(viridis(length(p) / 100), each = 100)[order(p)],
-  pch = ".",
-  cex = 8
-)
 
-results = data %>% 
-  cbind(p = p) %>% 
-  dplyr::group_by(site_id, lat, long) %>% 
-  summarise(count = sum(presence), p = mean(p)) 
+stopifnot(all(data$lat == sampling_events$lat))
 
-ggplot(results, aes(x = long, y = lat, color = count)) + geom_point(size = 4) + scale_color_viridis()
-ggplot(results, aes(x = long, y = lat, color = p)) + geom_point(size = 4) + scale_color_viridis()
+train_data = data %>% 
+  filter(year %in% train_years) %>%
+  inner_join(locations[ , c("fold_id", "site_id")], by = "site_id") %>%
+  dplyr::select(-lat, -long, -site_id, -year, -abundance)
+
+train_folds = train_data$fold_id
+
+train_data = dplyr::select(train_data, -fold_id)
+
+test_data = data %>% 
+  filter(year %in% test_years)
+
+cv_error = 0
+for (i in 1:n_folds) {
+  print(i)
+  # train on the k-1 folds that don't match i, validate on the 1 fold that does
+  fold_data = rbind(
+    train_data[i != train_folds, ], 
+    train_data[i == train_folds, ]
+  )
+  fold_model = gbm(
+    presence ~ .,
+    distribution = "bernoulli",
+    data = fold_data,
+    cv.folds = 0,
+    train.fraction =  mean(i != train_folds),
+    interaction.depth = 6,
+    n.trees = 4E3
+  )
+  cv_error = cv_error + fold_model$valid.error / n_folds
+}
