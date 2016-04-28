057a6bcc55efcc4a376eb14de017df8874e702d0
David J. Harris <harry491@gmail.com>
2016-04-28 16:27:25 -0400

    shear color tweaks
    Make the background less ugly, add threshold at exactly 0

--- make_shear_map.R
+++ make_shear_map.R
@@ -3,10 +3,11 @@ library(viridis)
 library(maps)
 
 # Assumes "results" is a data frame with long, lat, and p (which determines
-# intensity)
+# intensity and is between 0 and 1).
+# Note that any points with p==0 will not be plotted
 # Shear amount is how much to move the top to the right (so negative numbers
 # move to the left)
-make_shear_map = function(results, shear_amount = -1){
+make_shear_map = function(results, shear_amount = -1, map_color = "#FEFFEF"){
   shear = matrix(c(1, shear_amount, 0, 1), nrow = 2)
 
   results[ , c("x", "y")] = as.matrix(results[,c("long", "lat")]) %*% shear
@@ -26,12 +27,14 @@ make_shear_map = function(results, shear_amount = -1){
   corner_matrix = cbind(x = rep(xlim, each = 2), y = c(ylim, rev(ylim)))
   corners = corners = structure(as.data.frame(corner_matrix %*% shear), names = c("x", "y"))
 
+  results$alpha = ifelse(results$p == 0, 0, 1)
+
   ggplot(results, aes(x = x, y = y, color = p)) +
     geom_polygon(aes_(~x, ~y), data = corners, inherit.aes = FALSE, color = "black", fill = "white") +
-    shear_borders("state") +
-    geom_point(size = 2) +
-    scale_color_viridis(guide = FALSE) +
+    shear_borders("state", fill = map_color) +
+    geom_point(size = 2, alpha = results$alpha) +
+    scale_color_viridis(guide = FALSE, option = "magma", limits = c(0, 1),
+                        direction = -1) +
     coord_equal() +
     theme_void()
 }
-
