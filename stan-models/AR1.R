dd2ac6b03df7aff977b3e18479a5450524949e66
David J. Harris <harry491@gmail.com>
2016-09-08 12:53:53 -0400

    clean up AR1.R

--- stan-models/AR1.R
+++ stan-models/AR1.R
@@ -8,59 +8,82 @@ end_yr <- 2013
 min_num_yrs <- 25
 last_training_year <- 2003
 
-
+# The stan code expects the data to be sorted by site, then by year
 raw = get_richness_ts_env_data(start_yr, end_yr, min_num_yrs) %>%
-  select(site_id, year, richness, observer_id) %>%
   complete(site_id, year) %>%
   arrange(site_id, year)
 
 training_observations = filter(raw, year <= last_training_year)
 testing_observations = filter(raw, year > last_training_year)
 
-training_observations %>%
-  group_by(year, site_id) %>%
-  tidyr::spread(key = year, value = richness)
+n_train_years = length(unique(training_observations$year))
+n_test_years = length(unique(testing_observations$year))
 
-data = training_observations %>%
-  mutate(observation_index = 1:nrow(.)) %>%
-  rename(observed_richness = richness) %>%
-  filter(!is.na(observed_richness)) %>%
-  as.list()
 
-center = attr(scale(data$observed_richness), "scaled:center")
-scale = attr(scale(data$observed_richness), "scaled:scale")
+# Build "data" object to pass to Stan -------------------------------------
 
-# Observation-level pointers
-# Note that site-level random effect includes non-observed years, so its index
-# must as well
-data$observed_richness = c(scale(data$observed_richness))
+# The autoregressive model in Stan will work on one long vector of richness
+# estimates, one for each year/site combination (whether it was observed or
+# not). In the autoregressive model, each element that's not from year
+# `start_yr` will point back to the previous element. For each elment, we also
+# need to point to the site where the observation was taken and (if richness
+# data was collected) to the observer_id. Since the vector of observed richness
+# values will be shorter than the full vector (because of missing values), we
+# also need a vector/index to map from one to the other.
+
+data = training_observations
+
+# give site/year combination a number in the observation index, then throw out
+# the combinations that aren't used. This will be used to tell stan that ith
+# row of the filtered matrix corresponds to the jth row of the original matrix.
+data$observation_index = 1:nrow(data)
+data = data[!is.na(data$richness), ]
+
+# Convert observer_id to sequential integers to act as pointers in Stan.
 data$observer_index = as.integer(factor(data$observer_id))
-data$site_index = as.integer(factor(training_observations$site_id))
 
-# Vector lengths
-data$N_observations = length(data$observed)
+# Stan requires objects of different sizes in `data`, so rectangular tables
+# won't work anymore
+data = as.list(data)
+
+# For all site/year combinations (including those with no observations),
+# point to the corresponding site
 data$N_train_years = length(start_yr:last_training_year)
-data$N_test_years = length(seq(last_training_year + 1, end_yr))
+data$site_index = rep(seq_along(unique(data$site_id)),
+                      each = data$N_train_years)
+
+# Point to all the site/year combinations (including those with no observations)
+# that occurred in the first year of data collection, and separately to those
+# that occurred after the first year of data collection.
+year_mod = seq_along(data$site_index) %% data$N_train_years
+data$which_first = which(year_mod == 1)
+data$which_non_first = which(year_mod != 1)
+
+# Stan works better on z-scaled data.  Save the original mean and sd
+# for later
+data$scaled_richness = c(scale(data$richness))
+center = mean(data$richness)
+scale = sd(data$richness)
+
+# Stan wants to know in advance how long the above vectors are going to be.
+data$N_observations = length(data$scaled_richness)
 data$N_sites = length(unique(data$site_id))
 data$N_observers = max(data$observer_index)
 
-# year pointers (based on _all_ site/year combinations, not just ones with
-# observations)
-data$which_first = which(training_observations$year == start_yr)
-data$which_non_first = which(training_observations$year != start_yr)
+# Stan won't accept data named "long" because it's a reserved word.
+data$long = NULL
 
-# Drop unused variables
-data$site_id = NULL
-data$year = NULL
-data$observer_id = NULL
+# Compile the stan model --------------------------------------------------
 
-m = stan_model(
-  "stan-models/AR1.stan"
-)
+m = stan_model("stan-models/AR1.stan")
+stop()
+# Check the number of physical cores on the machine. With 2 or fewer cores,
+# just use one core.  Otherwise, use all of them.
+cores = ifelse(parallel::detectCores(logical = FALSE) <= 2, 1,
+               parallel::detectCores(logical = FALSE))
 
-# Check the number of physical cores on the machine. With 2 or fewer cores, just
-# use one core.  Otherwise, use all of them.
-cores = ifelse(parallel::detectCores(logical = FALSE) <= 2, 1, parallel::detectCores(logical = FALSE))
+
+# Sample from the stan model ----------------------------------------------
 
 model = sampling(m, data = data, control = list(adapt_delta = 0.9),
                  cores = cores, chains = 1, refresh = 1)
@@ -76,18 +99,36 @@ get_quantiles = function(x, q = c(.025, .975)){
 
 extracted = rstan::extract(model)
 
-expected = structure(extracted$y, dim = c(nrow(extracted$y), data$N_train_years, data$N_sites))
-#future_y = array(extracted$future_y, dim = c(nrow(extracted$future_y), data$N2, length(data$site_length)))
+expected = structure(extracted$y, dim = c(nrow(extracted$y), data$N_train_years,
+                                          data$N_sites))
+
+# Predict -----------------------------------------------------------------
+
+yy = expected[ , dim(expected)[2], ]
+future_y = array(NA, c(nrow(yy), ncol(yy), n_test_years))
+for (i in 1:n_test_years) {
+  future_means = extracted$site_alphas + extracted$site_betas * yy
+  yy = rnorm(length(yy), future_means, extracted$sigma_autoreg)
+  future_y[ , , i] = yy
+}
+
+# plot --------------------------------------------------------------------
 
 k = sample.int(dim(expected)[3], 1)
-matplot(start_yr:last_training_year, unscale(t(expected[,,k])), col = "#00000010", type = "l", lty = 1, ylab = "richness", xlab = "year", xlim = c(start_yr, end_yr), ylim = range(unscale(data$observed)))
-#matlines(seq(last_training_year+1, end_yr), unscale(t(future_y[,,k])), col = "#00000010", type = "l", lty = 1, ylab = "richness", xlab = "year")
-point_data = training_observations %>% filter(as.integer(factor(site_id)) == k) %>%
+predicted = unscale(t(future_y[,k,]))
+matplot(start_yr:end_yr,
+        rbind(unscale(t(expected[,,k])), predicted),
+        col = "#00000010", type = "l", lty = 1, ylab = "richness",
+        xlab = "year", xlim = c(start_yr, end_yr),
+        ylim = range(unscale(data$observed)))
+point_data = training_observations %>%
+  filter(as.integer(factor(site_id)) == k) %>%
   mutate(observer_id = factor(observer_id))
-point_data %>% lines(richness ~ year, data = ., col = 2, lwd = 2)
 point_data %>%
-  points(richness ~ year, data = ., col = 2 + as.integer(observer_id), pch = 16, ylim = range(unscale(data$observed)))
-
+  lines(richness ~ year, data = ., col = 2, lwd = 2)
+point_data %>%
+  points(richness ~ year, data = ., col = 2 + as.integer(observer_id),
+         pch = 16, ylim = range(unscale(data$observed)))
 
 # Vestigial pre-observer code ---------------------------------------------
 stop()
--- stan-models/AR1.stan
+++ stan-models/AR1.stan
@@ -2,7 +2,6 @@ data {
   // Sizes of vectors //
   int<lower=0> N_observations; // Total non-NA observations
   int<lower=0> N_train_years;  // Number of training years
-  int<lower=0> N_test_years;   // Number of test years
   int<lower=0> N_sites;
   int<lower=0> N_observers;
 
@@ -16,7 +15,7 @@ data {
   int<lower=0> which_non_first[N_sites * (N_train_years - 1)];
 
   // response variable (Z-scaled) //
-  real observed_richness[N_observations];
+  real scaled_richness[N_observations];
 }
 parameters {
   // Core autoregressive process model //
@@ -52,8 +51,8 @@ transformed parameters {
   // For non-first data points, what was richness the preceding year? //
   for (i in 1:num_elements(mu_non_first)) {
     int n;
-    // alpha + beta * y, where alpha and beta are for the _current site_, and
-    // y is for the _previous year_
+    // alpha + beta * y, where alpha and beta are for the _current site_,
+    // and y is for the _previous year_
     n = site_index[which_non_first[i]];
     mu_non_first[i] = site_alphas[n] +
       site_betas[n] * y[which_non_first[i] - 1];
@@ -68,7 +67,7 @@ model {
 
   // prior on global mean richness //
   mu_site[1] ~ normal(0, 0.1);     // must be close to observed global mean
-  // prior on autoregressive rate  //
+  // prior on autoregressive rate
   mu_site[2] ~ normal(0.5, 0.5);   // probably between 0 and 1
 
   // random effects //
@@ -76,37 +75,16 @@ model {
   observer_alpha ~ normal(0, sigma_observer);
 
   // Weak prior on y1 for when no observations were taken in year 1 //
+  // (sd==2 is a weak prior when the data is scaled to have sd==1)  //
   y[which_first] ~ normal(site_means[site_index[which_first]], 2);
 
   // Autoregression //
   y[which_non_first] ~ normal(mu_non_first, sigma_autoreg);
 
-  // Observation error
-  observed_richness ~ normal(y[observation_index] + observer_alpha[observer_index], nugget);
+  // Observation error //
+  scaled_richness ~ normal(
+    y[observation_index] + observer_alpha[observer_index],
+    nugget
+  );
 }
-// generated quantities {
-//   vector[N_test_years * N_sites] future_y;
-//   //vector[N_test_years * N_sites] future_observed;
-//
-//   for (i in 1:N_sites){
-//     int start;
-//     start = (i - 1) * N_test_years + 1;
-//
-//     for(j in 1:N_test_years){q
-//       real mu_temp;
-//       int to_update;
-//       to_update = start + j - 1;
-//
-//       if (j == 1){
-//         // First prediction is based on last value of future_y
-//         mu_temp = site_trend[i] + beta * y[i * N_train_years];
-//       } else{
-//         // subsequent predictions are based on previous value of future_y
-//         mu_temp = site_trend[i] + beta * future_y[to_update - 1];
-//       }
-//
-//       // future_y[to_update] = student_t_rng(nu, mu_temp +, sigma_autoreg);
-//       //future_observed[to_update] = normal_rng(future_y[to_update], nugget);
-//     }
-//   }
-// }
+
