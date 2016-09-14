33e16df6198537d4d51909eb8c511e3473d31960
David J. Harris <harry491@gmail.com>
2016-09-14 16:50:01 -0400

    even more fighting with hipergator/forecast package

--- stan-models/AR1.R
+++ stan-models/AR1.R
@@ -1,6 +1,6 @@
 if (as.numeric(packageDescription("forecast", fields = "Version")) < 7.1) {
   # Try to fix mysterious and persistent error about my version of "forecast"
-  install.packages("forecast", .libPaths()[2])
+  install.packages("forecast", .libPaths()[2], repos = "http://cloud.r-project.org/")
 }
 
 library(dplyr)
