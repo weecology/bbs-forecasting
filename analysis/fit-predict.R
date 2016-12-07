7f5d6ded44d9760b339252052d7c83a9c3a4f8be
David J. Harris <harry491@gmail.com>
2016-12-07 16:50:21 -0500

    make a functional for forecast funs

--- analysis/fit-predict.R
+++ analysis/fit-predict.R
@@ -1,7 +1,17 @@
+# If we only have one iteration, we want to say the variance is zero, not that
+# it's NA.
+safe_var = function(x){
+  if (length(x) == 1) {
+    0
+  } else {
+    var(x)
+  }
+}
+
 combine_predictions = function(x){
   x %>% 
     group_by(site_id, year, model, obs_model, richness) %>% 
-    summarize(sd = sqrt(mean(var(mean) + mean(sd^2))), 
+    summarize(sd = sqrt(mean(safe_var(mean) + mean(sd^2))), 
               mean = mean(mean)) %>% 
     ungroup()
 }
@@ -15,7 +25,7 @@ settings = yaml::yaml.load_file("settings.yaml")
 
 obs_model = readRDS("observer_model.rds")
 
-iters = 1:50 # During interactive use/testing, only include a few MCMC samples
+iters = 1:25 # During interactive use/testing, only include a few MCMC samples
 
 # `c()` is needed for a few lines because of distinction between 
 # vectors and 1D arrays, I think?
@@ -32,7 +42,6 @@ average_model_predictions = x_richness %>%
          model = "average", obs_model = TRUE) %>% 
   select(site_id, year, mean, sd, iteration, richness, model, obs_model)
 
-
 average_no_obs = x_richness %>% 
   filter(in_train) %>% 
   group_by(site_id) %>% 
@@ -42,47 +51,61 @@ average_no_obs = x_richness %>%
   select(site_id, year, mean, sd, richness, model, obs_model)
 
 
-make_naive_predictions = function(x){
+make_forecast = function(x, fun_name, obs_model, settings, ...){
+  if (obs_model) {
+    # observer effect will be added back in `make_all_forecasts`
+    response_variable = "expected_richness"
+  } else {
+    response_variable = "richness"
+  }
+  fun = getFromNamespace(fun_name, "forecast")
+  
   # Set the `level` so that `upper` and `lower` are 2 sd apart.
-  fc = forecast::naive(x$expected_richness, h = settings$end_yr - settings$last_train_year,
-                  level = pnorm(0.5))
+  fc = fun(x[[response_variable]], 
+         h = settings$end_yr - settings$last_train_year,
+         level = pnorm(0.5), ...)
   # Distance between `upper` and `lower` is 2 sd, so divide by 2
   tibble(year = seq(settings$last_train_year + 1, settings$end_yr), 
-         mean = c(fc$mean), sd = c(fc$upper - fc$lower) / 2, model = "naive",
-         obs_model = TRUE)
+         mean = c(fc$mean), sd = c(fc$upper - fc$lower) / 2, model = fun_name,
+         obs_model = obs_model)
 }
 
-naive_model_predictions = x_richness %>% 
-  filter(year <= settings$last_train_year) %>% 
-  complete(site_id, year) %>% 
-  group_by(site_id, iteration) %>% 
-  arrange(year) %>% 
-  by_slice(make_naive_predictions, .collate = "row") %>% 
-  left_join(select(x_richness, -sd), c("site_id", "year", "iteration")) %>% 
-  mutate(mean = mean + observer_effect) %>% 
-  select(site_id, year, mean, sd, iteration, richness, model, obs_model)
+make_all_forecasts = function(x, fun_name, obs_model, 
+                              settings, ...){
+  # `Forecast`` functions want NAs for missing years, and want the years in order
+  forecast_data = x %>% 
+    filter(year <= settings$last_train_year) %>% 
+    complete(site_id, year) %>%
+    group_by(site_id, iteration) %>%
+    arrange(year)
+  
+  if (!obs_model) {
+    # Without an observation model, all the iterations will be the same.
+    # Don't bother fitting the same model to each iteration
+    forecast_data = filter(forecast_data, iteration == 1)
+  }
+  
+  # TODO: by_slice will be deprecated. See if I can use mutate_all instead?
+  # Dropping x_richness$sd before joining so it can be replaced by forecast sd.
+  out = by_slice(forecast_data, make_forecast, fun_name = fun_name, 
+           obs_model = obs_model, settings = settings, ...,
+           .collate = "row") %>%
+    left_join(select(x_richness, -sd), c("site_id", "year", "iteration"))
+  
+  if (obs_model) {
+    # Observer effect was subtraced out in make_forcast. Add it back in here.
+    out = mutate(out, mean = mean + observer_effect)
+  }
 
-
-make_naive_predictions2 = function(x){
-  # Set the `level` so that `upper` and `lower` are 2 sd apart.
-  fc = forecast::naive(x$richness, h = settings$end_yr - settings$last_train_year,
-                       level = pnorm(0.5))
-  # Distance between `upper` and `lower` is 2 sd, so divide by 2
-  tibble(year = seq(settings$last_train_year + 1, settings$end_yr), 
-         mean = c(fc$mean), sd = c(fc$upper - fc$lower) / 2, model = "naive",
-         obs_model = FALSE)
+  select(out, site_id, year, mean, sd, iteration, richness, model, obs_model)
 }
-naive_no_obs = x_richness %>% 
-  filter(year <= settings$last_train_year) %>% 
-  complete(site_id, year) %>% 
-  group_by(site_id, iteration) %>% 
-  arrange(year) %>% 
-  by_slice(make_naive_predictions2, .collate = "row") %>% 
-  left_join(select(x_richness, -sd), c("site_id", "year", "iteration")) %>% 
-  mutate(mean = mean) %>% 
-  select(site_id, year, mean, sd, iteration, richness, model, obs_model)
 
-
+naive_model_predictions = make_all_forecasts(x_richness, "naive", 
+                                             obs_model = TRUE, 
+                                             settings = settings)
+naive_no_obs = make_all_forecasts(x_richness, "naive", 
+                                             obs_model = FALSE, 
+                                             settings = settings)
 
 
 make_gbm_predictions = function(x){
