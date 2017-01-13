43a08c533d745fb721500576edffc85228ad1a20
David J. Harris <harry491@gmail.com>
2017-01-13 15:10:19 -0500

    finally fix RF?

--- R/sdm-rf.R
+++ R/sdm-rf.R
@@ -1,38 +1,41 @@
 #' @importFrom randomForest randomForest
-one_rf_tree = function(bbs, vars, sp_id, iter, obs_model){
+one_rf_tree = function(bbs, vars, sp_id, iter, use_obs_model, x_richness,
+                       last_train_year){
   d = bbs %>% 
     select(site_id, year, species_id, abundance) %>% 
     filter(species_id == sp_id) %>% 
     distinct() %>% 
-    right_join(x_richness, by = c("site_id", "year"))
+    right_join(filter(x_richness, iteration == iter), 
+               by = c("site_id", "year")) %>% 
+    mutate(present = factor(ifelse(is.na(abundance), 0, 1)))
   
   my_formula = as.formula(paste("present ~", 
                                 paste(vars, collapse = "+")))
    
   rf = randomForest(
     my_formula, 
-    d %>% 
-      filter(in_train, iteration == iter) %>% 
-      mutate(present = factor(ifelse(is.na(abundance), 0, 1))),
+    filter(d, in_train),
     ntree = 1
   )  
   
   test = filter(d, !in_train, iteration == iter)
   test$mean = predict(rf, test, type = "prob")[,2]
-  test$obs_model = obs_model
+  test$use_obs_model = use_obs_model
   
-  select(test, site_id, year, species_id, mean, richness, obs_model, iteration)
+  select(test, site_id, year, species_id, mean, richness, use_obs_model, iteration)
 }
 
-rf_predict_species = function(sp_id, bbs, settings, x_richness, obs_model){
-  vars = c(settings$vars, if (obs_model) {"observer_effect"})
+rf_predict_species = function(sp_id, bbs, settings, x_richness, use_obs_model){
+  vars = c(settings$vars, if (use_obs_model) {"observer_effect"})
   iters = sort(unique(x_richness$iteration))
   results = lapply(iters, one_rf_tree,
-                   bbs = bbs, sp_id = sp_id, obs_model = obs_model, 
-                   vars = vars) %>% 
+                   bbs = bbs, sp_id = sp_id, use_obs_model = use_obs_model, 
+                   vars = vars,
+                   x_richness = x_richness,
+                   last_train_year = settings$last_train_year) %>% 
     bind_rows()
 
-  path = paste0("rf_predictions/sp_", sp_id, "_", obs_model, ".csv.gz")
+  path = paste0("rf_predictions/sp_", sp_id, "_", use_obs_model, ".csv.gz")
   dir.create("rf_predictions", showWarnings = FALSE)
   write.csv(results, file = gzfile(path), row.names = FALSE)
   
@@ -41,26 +44,31 @@ rf_predict_species = function(sp_id, bbs, settings, x_richness, obs_model){
     summarize(mean = mean(mean))
 }
 
-rf_predict_richness = function(bbs, x_richness, settings, obs_model, mc.cores) {
+rf_predict_richness = function(bbs, x_richness, settings, use_obs_model, mc.cores) {
   
   out = parallel::mclapply(
     unique(bbs$species_id), 
     function(sp_id){
       rf_predict_species(sp_id, bbs = bbs, x_richness = x_richness, 
                          settings = settings, 
-                         obs_model = obs_model)
+                         use_obs_model = use_obs_model)
     },
     mc.cores = mc.cores,
     mc.preschedule = FALSE
   ) %>% 
+    purrr::map(combine_sdm_iterations) %>% 
     bind_rows() %>% 
-    group_by(site_id, year, richness) %>% 
-    summarize(sd = sqrt(sum(mean * (1 - mean))), mean = sum(mean)) %>% 
+    group_by(site_id, year, richness, use_obs_model) %>% 
+    summarize(mean = sum(mean), sd = sqrt(sum(sd^2))) %>% 
     ungroup() %>% 
     mutate(model = "rf_sdm")
-  
-  # use standard evaluation because column name & function argument match
-  out$obs_model = obs_model
-  
+
   out
+}
+
+
+combine_sdm_iterations = function(d){
+  d %>% 
+    group_by(site_id, year, species_id, richness, use_obs_model) %>% 
+    summarize(mean = mean(mean), sd = sqrt(mean(mean * (1 - mean))))
 }
--- analysis/fit-rf.R
+++ analysis/fit-rf.R
@@ -4,12 +4,15 @@ devtools::load_all()
 settings = yaml::yaml.load_file("settings.yaml")
 
 if (!file.exists("observer_model.rds")) {
-  fit_observer_model()
+  obs_model = fit_observer_model()
 } else {
   obs_model = readRDS("observer_model.rds")
 }
 
-bbs = get_bbs_data()
+bbs = get_pop_ts_env_data(settings$start_yr, 
+                          settings$end_yr, 
+                          settings$min_num_yrs) %>% 
+  filter(!is.na(abundance))
 
 # Discard unnecessary data to save memory
 bioclim_to_discard = colnames(obs_model$data) %>% 
@@ -22,7 +25,7 @@ x_richness = obs_model$data %>%
   mutate(intercept = c(obs_model$intercept[iteration]), 
          sd = c(obs_model$sigma[iteration]),
          expected_richness = richness - observer_effect) %>% 
-  select(-ndvi_win, -ndvi_ann, -lat, -long, -site_index, -expected_richness, -sd,
+  select(-ndvi_ann, -lat, -long, -site_index, -expected_richness, -sd,
          -intercept, -site_effect, -one_of(bioclim_to_discard))
 
 rm(obs_model)
@@ -30,7 +33,7 @@ gc()
 
 
 rf_sdm_obs = rf_predict_richness(bbs = bbs, x_richness = x_richness, 
-                                 settings = settings, obs_model = TRUE,
+                                 settings = settings, use_obs_model = TRUE,
                                  mc.cores = 8)
 
 dir.create("rf_predictions", showWarnings = FALSE)
@@ -39,7 +42,7 @@ rm(rf_sdm_obs)
 gc()
 
 rf_sdm_no_obs = rf_predict_richness(bbs = bbs, x_richness = x_richness, 
-                                    settings = settings, obs_model = FALSE,
+                                    settings = settings, use_obs_model = FALSE,
                                     mc.cores = 8) 
 
 saveRDS(rf_sdm_no_obs, file = "rf_predictions/all_FALSE.rds")
