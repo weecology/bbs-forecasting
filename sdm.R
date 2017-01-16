e315bb61cad555e9ccd76a2c6267469e2bd04140
David J. Harris <harry491@gmail.com>
2017-01-16 10:15:24 -0500

    gbm SDM (deprecated)

--- sdm.R
+++ sdm.R
@@ -4,6 +4,13 @@ devtools::load_all()
 library(parallel)
 library(tidyverse)
 library(gbm)
+settings = yaml::yaml.load_file("settings.yaml")
+
+if (!file.exists("observer_model.rds")) {
+  fit_observer_model()
+} else {
+  obs_model = readRDS("observer_model.rds")
+}
 
 dir.create("sdm_predictions", showWarnings = FALSE)
 
@@ -14,58 +21,47 @@ first_validation_year = 2000 # for temporal cross-validation
 interaction.depth = 8
 n_trees_to_add = 1000
 max_trees = 1E4
-my_formula = present ~ bio2 + bio5 + bio15 + ndvi_sum + elevs +
-  observer_effect + site_effect
+my_formula = as.formula(paste("present ~", 
+                              paste(settings$vars, collapse = "+")))
 
 # Munge -------------------------------------------------------------------
-d = get_pop_ts_env_data(settings$start_yr, settings$end_yr, settings$min_num_yrs) %>% 
-  add_ranefs(last_training_year = settings$last_train_year)
+d = get_pop_ts_env_data(settings$start_yr, settings$end_yr, 
+                        settings$min_num_yrs) %>% 
+  filter(!is.na(species_id))
 
-train = d[d$year <= settings$last_train_year, ]
-test = d[d$year > settings$last_train_year, ]
+species_ids = unique(d$species_id)
 
-# Training data must be arranged by year first for temporal cross-validation
-# to work!
-train_x = select(train, -species_id, -abundance) %>% 
-  distinct() %>% 
-  filter(!is.na(lat)) %>% 
-  distinct() %>% 
-  arrange(year, site_id)
+sp_id = d$species_id[1]
+iter = 1
 
-test_x = select(test, -species_id, -abundance) %>% 
-  distinct() %>% 
-  filter(!is.na(lat)) %>% 
-  distinct() %>% 
-  arrange(year, site_id)
-
-species_ids = unique(train$species_id)
-
+all_data = d %>% 
+  filter(species_id == sp_id) %>% 
+  select(site_id, year, species_id, observer_id, abundance) %>% 
+  right_join(obs_model$data, by = c("site_id", "year", "observer_id")) %>% 
+  mutate(present = ifelse(is.na(abundance), 0, 1))
 
 # Fitting/predicting ------------------------------------------------------
-fit_species = function(sp_id){
-  cat(strsplit(date(), " ")[[1]][4], " fitting species", sp_id, "\n")
+fit_species = function(sp_id, iter, all_data){
+  cat(as.character(Sys.time()), ": fitting species", sp_id, "\n")
   
-  xy = filter(train, species_id == sp_id) %>% 
-    select(site_id, year) %>% 
-    mutate(present = 1) %>% 
-    right_join(train_x, c("site_id", "year")) %>% 
-    arrange(year, site_id) %>% 
-    mutate(present = ifelse(is.na(present), 0, 1))
+  # Training data must be arranged by year for temporal cross-validation
+  train = all_data %>% 
+    filter(in_train, iteration == iter) %>% 
+    arrange(year)
+  test = all_data %>% 
+    filter(!in_train, iteration == iter)
   
+  train_occupancy = train %>% 
+    filter(year < first_validation_year) %>% 
+    summarize(mean = mean(present), var = var(present))
+  
+  
+  message("think about 0-variance species")
   # avoid problems with 0% or 100% presence
-  if (mean(xy$present[xy$year < first_validation_year]) == 0) {
+  if (train_occupancy$var == 0) {
     return(
       list(
-        predictions = 0,
-        importances = NA,
-        n.trees = NA
-      )
-    )
-  }
-  if (mean(xy$present[xy$year < first_validation_year]) == 1) {
-    return(
-      list(
-        predictions = 1,
+        predictions = train_occupancy$mean,
         importances = NA,
         n.trees = NA
       )
@@ -77,18 +73,21 @@ fit_species = function(sp_id){
   done_adding = FALSE # flag for when we have enough trees
   probe_model = gbm(
     my_formula,
-    data = xy,
+    data = train,
     distribution = "bernoulli",
     interaction.depth = interaction.depth,
-    train.fraction = mean(xy$year < first_validation_year),
+    train.fraction = mean(train$year < first_validation_year),
+    shrinkage = 0.01,
     n.trees = 2 * n_trees_to_add
   )
   
   while (!done_adding) {
+    total_trees = probe_model$n.trees
     n.trees = gbm.perf(probe_model, method = "test", plot.it = FALSE)
     
-    # We're done adding trees when the optimal number of trees stops going up.
-    if (n.trees < (probe_model$n.trees - n_trees_to_add) | n.trees > max_trees) {
+    # We're done adding trees when the optimal number of trees stops going up
+    # or when we've reached the maximum number of trees
+    if (n.trees < (total_trees - n_trees_to_add) | total_trees >= max_trees) {
       done_adding = TRUE
     } else{
       probe_model = gbm.more(probe_model, n.new.trees = n_trees_to_add)
@@ -98,17 +97,18 @@ fit_species = function(sp_id){
   # Fit the full time series with the same number of trees
   prediction_model = gbm(
     my_formula,
-    data = xy,
+    data = train,
     distribution = "bernoulli",
     interaction.depth = interaction.depth,
     train.fraction = 1,
+    shrinkage = 0.01,
     n.trees = n.trees
   )
   
   list(
-    train_predictions = predict(probe_model, xy, n.trees = n.trees, 
+    train_predictions = predict(probe_model, train, n.trees = n.trees, 
                                 type = "response"),
-    test_predictions = predict(prediction_model, test_x, n.trees = n.trees, 
+    test_predictions = predict(prediction_model, test, n.trees = n.trees, 
                                type = "response"),
     importances = summary(prediction_model, plot = FALSE),
     n.trees = n.trees,
