68a97fbe3486a8953e3d35d135b3f48a2bf89f35
David J. Harris <harry491@gmail.com>
2017-01-25 16:11:05 -0500

    mostly-working JSDM

--- mistnet.R
+++ mistnet.R
@@ -7,11 +7,8 @@ settings = yaml::yaml.load_file("settings.yaml")
 
 if (!file.exists("observer_model.rds")) {
   fit_observer_model()
-} else {
-  obs_model = readRDS("observer_model.rds")
 }
-
-str(obs_model)
+obs_model = readRDS("observer_model.rds")
 
 bbs = get_bbs_data() %>% 
   mutate(species_id = paste0("sp", species_id), presence = abundance > 0) %>% 
@@ -23,13 +20,15 @@ i = 1
 data = filter(obs_model$data, iteration == i) %>% 
   left_join(bbs, c("site_id", "year"))
 
+vars = yaml::yaml.load_file("settings.yaml")$vars
+
 # cbinding because model.matrix wants a column called "y" (even though it won't
 # actually use it for anything).
 # Dropping the first column because the intercept is constant.
-x = model.matrix(as.formula(settings$formula), cbind(y = 1, data))[,-1] %>% 
-  cbind(observer_effect = data$observer_effect, site_effect = data$site_effect)
-
-
+x = data %>% 
+  select(one_of(vars)) %>% 
+  as.matrix() %>% 
+  cbind(observer_effect = data$observer_effect)
 # Scaling incorrectly because it's quicker for now
 x = scale(x)
 
@@ -46,7 +45,6 @@ rate = 0.1
 latent_dim = 6L
 n.importance.samples = 30L
 n.minibatch = 100L
-
 
 net = mistnet(
   x = x[data$in_train, ],
@@ -77,10 +75,11 @@ net = mistnet(
   initialize.biases = TRUE,
   initialize.weights = TRUE
 )
-net$layers[[1]]$biases[] = 1 # First layer biases equal 1
+# First layer biases equal 1; prevents "dead" rectifiers
+net$layers[[1]]$biases[] = 1
 
 
-while (TRUE) {
+while (net$completed.iterations <= 1E3) {
   net$fit(10)
   cat(".")
   # Update prior variance
@@ -106,7 +105,20 @@ while (TRUE) {
 
 
 hist(net$layers[[3]]$outputs, breaks = "fd", border = 2, yaxs = "i")
+hist(qlogis(apply(net$layers[[3]]$outputs, 2, rowMeans)), breaks = "fd", border = 2, yaxs = "i")
 hist(net$layers[[3]]$coef.updater$delta, breaks = "fd", border = 2)
 
-p = predict(net, newdata = x[!data$in_train, ], n.importance.samples = 10)
-Metrics::mse(rowSums(apply(p, 2, rowMeans)), rowSums(y[!data$in_train, ]))
+n_samples = 20
+p = predict(net, newdata = x[!data$in_train, ], n.importance.samples = n_samples)
+
+make_mistnet_tidy = function(p, value_name){
+  t(apply(p, 1, colSums)) %>% 
+    cbind(data[!data$in_train, c("site_id", "year", "iteration")]) %>% 
+    gather_(key_col = "sample", value_col = value_name, 
+            gather_cols = as.character(1:n_samples))
+}
+
+out = make_mistnet_tidy(p, "mean") %>% 
+  left_join(make_mistnet_tidy(p * (1 - p), "variance"),
+            c("site_id", "year", "iteration", "sample")) %>% 
+  left_join(select(data, site_id, year, richness), c("site_id", "year"))
