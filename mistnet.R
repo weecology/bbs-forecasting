17b289191837625688907c21bab451cc0f79155b
David J. Harris <harry491@gmail.com>
2017-01-29 13:18:33 -0500

    pull mistnet model into a function

--- mistnet.R
+++ mistnet.R
@@ -2,171 +2,183 @@ library(tidyverse)
 library(mistnet)
 library(progress)
 devtools::load_all()
-settings = yaml::yaml.load_file("settings.yaml")
-n_prediction_samples = 20
-use_obs_model = TRUE
 
 if (!file.exists("observer_model.rds")) {
   fit_observer_model()
 }
-obs_model = readRDS("observer_model.rds")
 
 bbs = get_bbs_data() %>% 
   mutate(species_id = paste0("sp", species_id), presence = abundance > 0) %>% 
   select(-lat, -long, -abundance) %>% 
   spread(key = species_id, value = presence, fill = 0)
 
-
-i = 1
-data = filter(obs_model$data, iteration == i) %>% 
-  left_join(bbs, c("site_id", "year"))
-
-# Free up some memory
-rm(bbs, obs_model)
-gc()
-
-vars = c(settings$vars, if (use_obs_model) {"observer_effect"})
-
-x = data %>% 
-  select(one_of(vars)) %>% 
-  as.matrix()
-# Scaling incorrectly because it's quicker for now
-x = scale(x)
-
-y = data %>% 
-  select(matches("^sp[0-9]+$")) %>% 
-  select(which(colSums(.) > 0)) %>% 
-  as.matrix()
-
-# Drop columns we won't need below
-data = data %>% 
-  select(site_id, year, iteration, richness, in_train)
-gc()
-
-# Model definition --------------------------------------------------------
-# Based on the second-best model in Appendix D of the mistnet paper
+# Mistnet settings based on the second-best model in Appendix D of 
+# the mistnet paper
 n.minibatch = 23L
 latent_dim = 6L
 n.importance.samples = 24L
-N1 = 35L
+N1 = 100L
 N2 = 12L
 
-net = mistnet(
-  x = x[data$in_train, ],
-  y = y[data$in_train, ],
-  layer.definitions = list(
-    defineLayer(
-      nonlinearity = rectify.nonlinearity(),
-      size = N1,
-      prior = gaussian.prior(mean = 0, sd = .5)
-    ),
-    defineLayer(
-      nonlinearity = linear.nonlinearity(),
-      size = N2,
-      prior = gaussian.prior(mean = 0, sd = .5)
-    ),
-    defineLayer(
-      nonlinearity = sigmoid.nonlinearity(),
-      size = ncol(y),
-      prior = gaussian.prior(mean = 0, sd = .5)
-    )
-  ),
-  loss = bernoulliRegLoss(a = 1 + 1E-6, b = 1 + 1E-6),
-  updater = adam.updater(a_0 = 0.01, b2 = 0.99, annealing = .9995),  
-  sampler = gaussian.sampler(ncol = latent_dim, sd = 1),
-  n.importance.samples = n.importance.samples,
-  n.minibatch = n.minibatch,
-  training.iterations = 0,
-  initialize.biases = TRUE,
-  initialize.weights = TRUE
-)
-# First layer biases equal 1; prevents "dead" rectifiers
-net$layers[[1]]$biases[] = 1
-
-# Shrink each layer's initail weights
-net$layers[[1]]$weights = net$layers[[1]]$weights / 2
-net$layers[[2]]$weights = net$layers[[2]]$weights / 2
-net$layers[[3]]$weights = net$layers[[3]]$weights / 2
-
-# save weights
-initial_weights = net$layers %>% map(~.x[["weights"]])
-
-# Fit the model -----------------------------------------------------------
+# Other mistnet settings
 n_opt_iterations = 1E4
 iteration_size = 10
-pb = progress_bar$new(format = "[:bar] :percent eta: :eta",
-                      total = n_opt_iterations / iteration_size)
-pb$tick(0)
-while (net$completed.iterations < n_opt_iterations) {
-  net$fit(iteration_size)
-  pb$tick()
-  # Update prior variance
-  for (layer in net$layers) {
-    layer$prior$update(
+n_prediction_samples = 20
+
+fit_mistnet = function(iter,
+                       use_obs_model){
+  
+  settings = yaml::yaml.load_file("settings.yaml")
+  
+  # Collect data for this iteration, then discard the rest to save
+  # memory
+  obs_model = readRDS("observer_model.rds")
+  data = filter(obs_model$data, iteration == iter) %>% 
+    left_join(bbs, c("site_id", "year"))
+  rm(obs_model)
+  gc()
+  
+  vars = c(settings$vars, if (use_obs_model) {"observer_effect"})
+  
+  x = data %>% 
+    select(one_of(vars)) %>% 
+    as.matrix()
+  
+  # Z-scale each column based on mean & sd of training set
+  for (i in 1:ncol(x)) {
+    mean_i = mean(x[data$in_train, i])
+    sd_i = sd(x[data$in_train, i])
+    x[,i] = (x[,i] - mean_i) / sd_i
+  }
+  
+  y = data %>% 
+    select(matches("^sp[0-9]+$")) %>% 
+    select(which(colSums(.) > 0)) %>% 
+    as.matrix()
+  
+  # Drop columns we won't need below
+  data = data %>% 
+    select(site_id, year, iteration, richness, in_train)
+  gc()
+  
+  # Model definition --------------------------------------------------------
+  
+  net = mistnet(
+    x = x[data$in_train, ],
+    y = y[data$in_train, ],
+    layer.definitions = list(
+      defineLayer(
+        nonlinearity = rectify.nonlinearity(),
+        size = N1,
+        prior = gaussian.prior(mean = 0, sd = .5)
+      ),
+      defineLayer(
+        nonlinearity = linear.nonlinearity(),
+        size = N2,
+        prior = gaussian.prior(mean = 0, sd = .5)
+      ),
+      defineLayer(
+        nonlinearity = sigmoid.nonlinearity(),
+        size = ncol(y),
+        prior = gaussian.prior(mean = 0, sd = .5)
+      )
+    ),
+    loss = bernoulliRegLoss(a = 1 + 1E-6, b = 1 + 1E-6),
+    updater = adam.updater(a_0 = 0.01, b2 = 0.99, annealing = .9995),  
+    sampler = gaussian.sampler(ncol = latent_dim, sd = 1),
+    n.importance.samples = n.importance.samples,
+    n.minibatch = n.minibatch,
+    training.iterations = 0,
+    initialize.biases = TRUE,
+    initialize.weights = TRUE
+  )
+  # First layer biases equal 1; prevents "dead" rectifiers
+  net$layers[[1]]$biases[] = 1
+  
+  # Shrink each layer's initial weights compared to baseline
+  net$layers[[1]]$weights = net$layers[[1]]$weights / 2
+  net$layers[[2]]$weights = net$layers[[2]]$weights / 2
+  net$layers[[3]]$weights = net$layers[[3]]$weights / 2
+  
+  # Fit the model ---------------------------------------------------------
+  pb = progress_bar$new(format = "[:bar] :percent eta: :eta",
+                        total = n_opt_iterations / iteration_size)
+  pb$tick(0)
+  while (net$completed.iterations < n_opt_iterations) {
+    net$fit(iteration_size)
+    pb$tick()
+    # Update prior variance
+    for (layer in net$layers) {
+      layer$prior$update(
+        layer$weights, 
+        update.mean = FALSE, 
+        update.sd = TRUE,
+        min.sd = .01
+      )
+    }
+    # Update mean for final layer
+    net$layers[[3]]$prior$update(
       layer$weights, 
-      update.mean = FALSE, 
-      update.sd = TRUE,
+      update.mean = TRUE, 
+      update.sd = FALSE,
       min.sd = .01
     )
-  }
-  # Update mean for final layer
-  net$layers[[3]]$prior$update(
-    layer$weights, 
-    update.mean = TRUE, 
-    update.sd = FALSE,
-    min.sd = .01
-  )
-  if (any(is.na(net$layers[[3]]$outputs))) {
-    stop("NA in network output")
-  }
-  ll = sapply(1:n.importance.samples,
-              function(i){
-                net$loss$loss(y = net$y[net$row.selector$minibatch.ids, ], 
-                              yhat = net$layers[[3]]$outputs[,,i]) %>% 
-                  rowSums()
-              })
-  print(mean(rowSums(ll * net$importance.weights)))
-} # End while
-
-p = predict(net, 
-            newdata = x[!data$in_train, ], 
-            n.importance.samples = n_prediction_samples)
-
-make_mistnet_tidy = function(p, value_name, data, n_prediction_samples){
-  # Add up p:
-  #  Sum of probabilities is expected richness
-  #  Sum of variances is total variance
-  sums = t(apply(p, 1, colSums)) 
+    if (any(is.na(net$layers[[3]]$outputs))) {
+      stop("NA in network output")
+    }
+  } # End while
   
-  # Add the other columns, convert to long format
-  column_names = c("site_id", "year", "iteration", "richness")
-  sums %>%
-    cbind(data[!data$in_train, column_names]) %>%
-    gather_(key_col = "sample", 
-            value_col = value_name,
-            gather_cols = as.character(1:n_prediction_samples),
-            convert = TRUE)
+  p = predict(net, 
+              newdata = x[!data$in_train, ], 
+              n.importance.samples = n_prediction_samples)
+  
+  # One richness point estimate & uncertainty per Monte Carlo draw
+  richness_estimates = t(apply(p, 1, colSums))
+  variance_estimates = t(apply(p * (1 - p), 1, colSums))
+  
+  # total variance is variance in mean estimates plus mean of the variance 
+  # estimates
+  out = data %>% 
+    filter(!in_train) %>% 
+    cbind(mean = rowMeans(richness_estimates),
+          sd = sqrt(apply(richness_estimates, 1, var) + 
+                      rowMeans(variance_estimates))) %>% 
+    select(-in_train) %>% 
+    mutate(model = "mistnet", use_obs_model = use_obs_model)
+  
+  dir.create("mistnet_output", showWarnings = FALSE)
+  saveRDS(out, file = paste0("mistnet_output/", "iteration_", iter, 
+                             "_", use_obs_model, ".rds"))
 }
 
-# Join the mean data frame to the variance data frame, then summarize
-# and tack on extra columns
-out = make_mistnet_tidy(p, "mean", data, 
-                        n_prediction_samples) %>%
-  left_join(make_mistnet_tidy(p * (1 - p), "variance", data, 
-                              n_prediction_samples),
-            c("site_id", "year", "iteration", "sample","richness")) %>% 
-  group_by(site_id, year, iteration, richness) %>% 
-  summarize(sd = sqrt(mean(variance) + var(mean)), 
-            mean = mean(mean)) %>% 
-  ungroup() %>% 
-  mutate(model = "mistnet", use_obs_model = use_obs_model)
-  
-dir.create("mistnet_output", showWarnings = FALSE)
-saveRDS(out, file = paste0("mistnet_output/", "iteration_", i, ".rds"))
+# # Plotting species' responses to environmental variables
+# N = 250
+# xy = as.matrix(expand.grid(seq(-4, 4, length = N), seq(-4, 4, length = N)))
+# xx = cbind(as.matrix(x[rep(1, N^2), ]), matrix(0, N^2, latent_dim))
+# xx[, c(3, 11)] = xy
+# 
+# relu = function(x){
+#   structure(pmax(x, 0), dim = dim(x))
+# }
+# 
+# to_plot = xx %*% 
+#   net$layers[[1]]$weights %>% 
+#   relu() %*% 
+#   net$layers[[2]]$weights %*% net$layers[[3]]$weights %>% 
+#   plogis()
+# 
+# cbind(xy, fill = to_plot[,28]) %>% 
+#   as.data.frame() %>% 
+#   ggplot(aes(x = Var1, y = Var2, fill = fill)) + 
+#   geom_raster() +
+#   viridis::scale_fill_viridis(limits = c(0, 1)) + 
+#   cowplot::theme_cowplot()
 
+# Summarizing mean squared error
 # out %>%
 #   group_by(year, site_id, richness) %>%
 #   summarize(mean = mean(mean)) %>%
 #   summarize(mse = mean((richness - mean)^2)) %>%
 #   group_by(year) %>%
 #   summarize(mse = mean(mse))
+
