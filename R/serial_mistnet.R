4a59de56eeac6d564579f3123cedb28da1037b46
David J. Harris <harry491@gmail.com>
2017-03-26 13:20:52 -0400

    Start many separate mistnet jobs

---
+++ R/serial_mistnet.R
@@ -0,0 +1,46 @@
+N_files = 500
+N_jobs = 25
+
+files_per_job = N_files / N_jobs
+
+starts = seq(0, N_jobs - 1) * files_per_job + 1
+ends = starts + files_per_job - 1
+
+script = "#!/bin/bash
+  
+# Job name and who to send updates to
+#SBATCH --job-name=mistnet
+#SBATCH --mail-user=harris.d@ufl.edu
+#SBATCH --mail-type=FAIL,END
+
+# Where to put the outputs:
+#   %A expands to the job-name specified above
+#   %j expands into the job number (a unique identifier for this job)
+#SBATCH --output mistnet-%j.out
+#SBATCH --error mistnet-%j.err
+
+# Number of nodes to use, following https://wiki.rc.ufl.edu/doc/R#FAQ
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+
+# Memory per cpu core. Default is megabytes, but units can be specified 
+# with M or G for megabytes or Gigabytes.
+#SBATCH --mem-per-cpu=8G
+
+# Job run time in [DAYS]
+# HOURS:MINUTES:SECONDS
+# [DAYS] are optional, use when it is convenient
+#SBATCH --time=30-00:00:00
+
+# Save some useful information to the 'output' file
+date;hostname;pwd
+
+# Load R and run a script
+module load R
+Rscript --default-packages=stats,graphics,grDevices,utils,methods 'analysis/fit-mistnet.R'"
+
+for (i in 1:N_jobs) {
+  # send the script to SLURM with the specified starts & ends as arguments
+  system(paste("sbatch", script, "--args", starts[[i]], ends[[i]]))
+}
--- analysis/fit-mistnet.R
+++ analysis/fit-mistnet.R
@@ -1,15 +1,12 @@
-print(mistnet::adam.updater()$annealing_rate) # will fail if mistnet version is old
+args <- commandArgs(TRUE)
+start = args[[1]]
+end = args[[2]]
 
-library(doParallel)
-cl=makeCluster(16)
-registerDoParallel(cl)
+# check for mistnet version: this will fail if mistnet version is too old
+invisible(mistnet::adam.updater()$annealing_rate)
 
-foreach(i=1:500) %dopar% {
-  source("mistnet.R")
+for (i in start:end) {
+  print(i)
   fit_mistnet(iter = i, use_obs_model = TRUE)
-}
-
-foreach(i=1:500) %dopar% {
-  source("mistnet.R")
   fit_mistnet(iter = i, use_obs_model = FALSE)
 }
