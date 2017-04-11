b43bc6d010179ad5080bf19339e6baef025354c3
Ethan White <ethan@weecology.org>
2017-04-11 14:16:50 -0400

    Add new packages to package install script

--- install-packages.R
+++ install-packages.R
@@ -10,7 +10,7 @@ pacman::p_load(dplyr, forecast, ggplot2, Hmisc, tidyr, mgcv, sp, raster,
 	       broom, devtools, doParallel, dplyr, forecast, ggplot2,
 	       gimms, Hmisc, maptools, mgcv, prism, raster, stringr, sp,
 	       tidyr, rgdal, rgeos, DBI, RSQLite, lme4, caret, mapproj,
-               viridis, git2r)
+               viridis, git2r, rstan, readr, purr, gbm, randomForest)
 pacman::p_load_gh('seantuck12/MODISTools', 'ropensci/ecoretriever')
 
 
