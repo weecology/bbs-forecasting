25efa958fa847c502084047d90198e1b0eb910b8
Shawn <shawntaylor@weecology.org>
2017-06-15 11:02:05 -0400

    update details

--- process_bcsd_data.py
+++ process_bcsd_data.py
@@ -7,16 +7,16 @@ def process_data(filename, new_filename):
     ds = xr.open_dataset(filename)
 
     #Make a single time series ensemble
-    #of all the different ensembles
+    #of all the different model runs
     ds = ds.mean('projection')
 
     #Longitude here is from 0 - 360, adjust to -180 - 180
-    #Note if any data was outside the western hemisphere this
+    #Note that if any data was outside the western hemisphere this
     #would have to done differently
     adjusted_longitude = ds['longitude'].values - 360
     ds['longitude'].values = adjusted_longitude
 
     ds.to_netcdf(new_filename)
 
-process_data('data/cmip5/bcsd5/Extraction_pr.nc', 'data/cmip5/cmip5_precip.nc')
-process_data('data/cmip5/bcsd5/Extraction_tas.nc', 'data/cmip5/cmip5_temp.nc')
+process_data('data/cmip5/bcsd5_pr/Extraction_pr.nc', 'data/cmip5/cmip5_precip.nc')
+process_data('data/cmip5/bcsd5_temp/Extraction_tas.nc', 'data/cmip5/cmip5_temp.nc')
